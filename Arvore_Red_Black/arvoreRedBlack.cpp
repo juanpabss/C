06a9c1a844c7c55313513fd0be7f074dd4be6ed7
JuanPabss <juanpablodev2@gmail.com>
2024-08-06 00:10:19 -0300

    implementação da struct e função cor

--- Arvore_Red_Black/arvoreRedBlack.cpp
+++ Arvore_Red_Black/arvoreRedBlack.cpp
@@ -1,3 +1,25 @@
 //
 // Created by juanp on 05/08/2024.
 //
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define RED 1
+#define BLACK 0
+
+typedef struct No{
+   int valor;
+   struct No *esquerda;
+   struct No *direita;
+   int cor;
+}No;
+
+int cor(No *raiz){
+    if(raiz == NULL){
+        return BLACK;
+    }else{
+        return raiz->cor;
+    }
+}
+
